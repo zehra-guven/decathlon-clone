c33e73ded5617704fea9756a990e31dd016d5dcc
zehra-guven <150480599+zehra-guven@users.noreply.github.com>
2025-02-04 14:52:41 +0300

    updated api key

--- firebaseConfig.ts
+++ firebaseConfig.ts
@@ -4,14 +4,13 @@ import { getFirestore } from 'firebase/firestore';
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "***************************************",
-    authDomain: "***********************************",
-    projectId: "************************************",
-    storageBucket: "********************************",
-    messagingSenderId: "****************************",
-    appId: "****************************************",
-    measurementId: "*********************************"
-  };
+  apiKey: "AIzaSyDI014ti6U6PdmRiyfwHWiWeOpD5X_9508",
+  authDomain: "decathlonclone-315ad.firebaseapp.com",
+  projectId: "decathlonclone-315ad",
+  storageBucket: "decathlonclone-315ad.firebasestorage.app",
+  messagingSenderId: "31853298845",
+  appId: "1:31853298845:web:96274f4d1766c578248074"
+};
 
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
